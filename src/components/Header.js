51327285dd2e517c3a65bbcd20fe6af71cb9733f
shaikhadnan7621 <shaikhadnan7621@gmail.com>
2024-08-09 16:33:56 +0000

    tested new things

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,22 +12,22 @@ export default function Header() {
     setShowMenu(!showMenu);
   };
   const handleLinkClick = () => {
-    setShowMenu(false); // Close the menu
+    setShowMenu(false); 
   };
 
   return (
     <header >
       <div className="absolute top-0 w-full z-50 py-1 px-4 max-w-[1280px] container mx-auto flex items-center justify-between left-1/2 transform -translate-x-1/2 text-green-700" >
         <Link href="/">
-          <h1 className=" text-2xl font-bold">PROPrompter</h1>
+          <h1 className={`text-2xl font-bold ${!showMenu ? "text-green-700" : "text-white"}`}>PROPrompter</h1>
         </Link>
 
         <div className="sm:hidden">
-          <button onClick={toggleMenu} className=" focus:outline-none">
+          <button onClick={toggleMenu} className=" focus:outline-none align-middle">
             {showMenu ? (
-              <FaTimes className="text-2xl" />
+              <FaTimes className={` text-2xl text-white`} />
             ) : (
-              <FaBars className="text-2xl" />
+              <FaBars className={` text-2xl  text-green-700`} />
             )}
           </button>
         </div>
@@ -35,16 +35,16 @@ export default function Header() {
         <nav className="hidden sm:flex sm:items-center sm:space-x-6  " >
           <ul className="sm:flex sm:items-center sm:space-x-6 ">
             <li>
-              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold ' href="/prompts/galary" onClick={handleLinkClick}>Gallery</Link>
+              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold ' href="/prompts/galary" >Gallery</Link>
             </li>
             <li>
-              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold ' href="/prompts/myprompts" onClick={handleLinkClick}>My Prompts</Link>
+              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold ' href="/prompts/myprompts" >My Prompts</Link>
             </li>
             <li>
-              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold ' href="/chat" onClick={handleLinkClick}>Chat Now</Link>
-            </li>
+              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold ' href="/chat" >Chat Now</Link>
+            </li> 
             <li>
-              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold ' href="/auth/login" onClick={handleLinkClick}>Login</Link>
+              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold ' href="/auth/login" >Login</Link>
             </li>
           </ul>
         </nav>
@@ -52,19 +52,19 @@ export default function Header() {
       </div>
       {
         showMenu &&
-        <nav className="absolute top-0 w-full sm:flex sm:items-center sm:space-x-6   z-40 p-4">
+        <nav className="absolute top-0 w-full sm:flex sm:items-center sm:space-x-6 z-10 text-center p-4 bg-green-700 text-white">
           <ul className="sm:flex sm:items-center sm:space-x-6 space-y-3  mt-10">
             <li>
-              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold ' href="/prompts/galary" onClick={handleLinkClick}>Gallery</Link>
+              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold text-center ' href="/prompts/galary" onClick={handleLinkClick}>Gallery</Link>
             </li>
             <li>
-              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold ' href="/prompts/myprompts" onClick={handleLinkClick}>My Prompts</Link>
+              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold text-center ' href="/prompts/myprompts" onClick={handleLinkClick}>My Prompts</Link>
             </li>
             <li>
-              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold ' href="/chat" onClick={handleLinkClick}>Chat Now</Link>
+              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold text-center ' href="/chat" onClick={handleLinkClick} >Chat Now</Link>
             </li>
             <li>
-              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold ' href="/auth/login" onClick={handleLinkClick}>Login</Link>
+              <Link className='hover:border-b-2 duration-75 hover:pb-0.5 hover:border-green-700 hover:font-bold text-center ' href="/auth/login" onClick={handleLinkClick} >Login</Link>
             </li>
           </ul>
         </nav>
--- src/components/ReactMDView.js
+++ src/components/ReactMDView.js
@@ -1,27 +1,10 @@
-// //  components/MarkdownViewer.js
+//  components/MarkdownViewer.js
 // import Markdown from 'markdown-to-jsx';
 // export default function MarkdownViewer({ prompt }) {
-
 //   return (
-//     <div className='p-1 pb-0 min-h-min overflow-auto moderscroller '
-//     >
-//       <Markdown options={{
-//         overrides: {
-//           ul: {
-//             component: 'ul',
-//             props: {
-//               className: 'list-disc list-inside' //  Tailwind for unordered lists
-//             }
-//           },
-//           ol: {
-//             component: 'ol',
-//             props: {
-//               className: 'list-decimal list-inside' // Tailwind for ordered lists
-//             }
-//           }
-//         }
-//       }}>
-//         {prompt.replace(/\n\n/g, '<br><br>\n')}
+//     <div className='p-1 pb-0 min-h-min overflow-auto moderscroller'>
+//       <Markdown className="markdown-container">
+//         {prompt}
 //       </Markdown>
 //     </div>
 //   );
@@ -29,16 +12,13 @@
 
 
 import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
+import  remarkGfm from 'remark-gfm';
 export default function MarkdownViewer({ prompt }) {
   return (
-    <div className='p-1 pb-0 min-h-min overflow-auto moderscroller whitespace-none'
-    >
-      <ReactMarkdown remarkPlugins={[remarkGfm]}
-        className="markdown-container "
-      >
+    <div className="p-1 pb-0 min-h-min overflow-auto moderscroller whitespace-none">
+      <ReactMarkdown remarkPlugins={[remarkGfm]} className="markdown-container">
         {prompt}
       </ReactMarkdown>
-    </div >
-  );
-} 
+    </div>
+  )
+}
